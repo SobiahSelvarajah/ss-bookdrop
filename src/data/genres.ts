688b2bfaf88aa958178165ee258bc080c6fa170f
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-07-20 21:12:48 +0100

    feat: genres data added to data folder

---
+++ src/data/genres.ts
@@ -0,0 +1,34 @@
+
+
+export const genres = [
+    {
+        title: "Mystery",
+        description: 
+            "Twisting plots, clever detectives and page-turning suspense.",
+    },
+    {
+        title: "Fantasy",
+        description: 
+            "Epic adventures, magical worlds and unforgettable heroes.",
+    },
+    {
+        title: "Romance",
+        description: 
+            "Heartwarming love stories, emotional journeys and happy endings.",
+    },
+    {
+        title: "Science Fiction",
+        description: 
+            "Explore futuristic technology, space travel and imagined worlds.",
+    },
+    {
+        title: "Historical Fiction",
+        description: 
+            "Experience captivating stories inspired by real moments in history.",
+    },
+    {
+        title: "Thriller",
+        description: 
+            "High stakes tension, unexpected twists and edge-of-your-seat action.",
+    },
+];
