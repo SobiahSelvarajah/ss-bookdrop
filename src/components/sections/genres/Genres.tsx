fa28ea2dd011943707cdcf958a2d5420929c32fe
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-07-21 13:07:20 +0100

    refactor: GenresCard component is now reusable, rendered with genres data file and connected with Genres component

--- src/components/sections/genres/Genres.tsx
+++ src/components/sections/genres/Genres.tsx
@@ -1,5 +1,6 @@
 import GenresIntro from "./GenresIntro";
 import GenresCard from "./GenresCard";
+import { genres } from "@/data/genres";
 
 
 export default function Genres() {
@@ -7,8 +8,14 @@ export default function Genres() {
         <section id="genres" className="px-8 py-24">
             <div className="mx-auto max-w-7xl">
                 <GenresIntro />
-                <div>
-                    <GenresCard />
+                <div className="mt-16 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                    {genres.map((genre) => (
+                        <GenresCard 
+                            key={genre.title}
+                            title={genre.title}
+                            description={genre.description}
+                        />
+                    ))}
                 </div>
             </div>
         </section>
