c97011103c79cb83d6e161bd9e6e97e0b907e153
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-07-21 21:30:11 +0100

    feat: pricing data added to data folder

---
+++ src/data/pricing.ts
@@ -0,0 +1,46 @@
+
+
+export const pricing = [
+    {
+        name: "Free Week",
+        price: "£0",
+        period: "7-day trial",
+        description: "Experience BookDrop with a free week of personalised book recommendations.",
+        features: [
+            "One curated recommendation",
+            "Choose your favourite genre",
+            "Delivered to your inbox",
+            "Cancel anytime",
+        ],
+        buttonText: "Start Free Week",
+        featured: false,
+    },
+    {
+        name: "Monthly",
+        price: "£4.99",
+        period: "per month",
+        description: "Enjoy unlimited weekly recommendations tailored to your favourite genres.",
+        features: [
+            "Weekly curated recommendations",
+            "Unlimited genre changes",
+            "Exclusive reading picks",
+            "Priority email support",
+        ],
+        buttonText: "Start Monthly Plan",
+        featured: true,
+    },
+    {
+        name: "Annual",
+        price: "£49.99",
+        period: "per year",
+        description: "Save more with a yearly membership while enjoying uninterrupted recommendations.",
+        features: [
+            "Everything in Monthly",
+            "Save over 15%",
+            "Early access to new features",
+            "Annual members' exclusives",
+        ],
+        buttonText: "Start Annual Plan",
+        featured: false, 
+    },
+];
