9f2c9c8ec07b85452aedd3a2ad2e22704f1faf21
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-08-05 11:47:29 +0100

    feat: subscription server action added to actions folder

---
+++ src/actions/subscribe.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { subscriptionSchema } from "@/lib/validation/subscriptionSchema";
+
+
+export async function subscribe(data: unknown) {
+    const validated = subscriptionSchema.safeParse(data);
+
+    if (!validated.success) {
+        return {
+            success: false,
+        };
+    }
+
+    console.log(validated.data);
+
+    return {
+        success: true,
+    };
+}
