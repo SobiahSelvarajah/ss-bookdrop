e63147face53d9b395b2707cd52c090c52a191c3
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-08-27 20:39:22 +0100

    fix: smooth scroll behaviour declared in the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behaviour="smooth">
       <body className={cn(geist.variable, cormorant.variable)}>
         <Navbar />
         {children}
